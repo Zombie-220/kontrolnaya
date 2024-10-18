a3d37d46be246cbc52d53d38ccb7728a4df38a77
Zombie-220 <3om6n_220@mail.ru>
2024-10-19 01:09:07 +0700

    header

---
+++ src/navigation/errorPage/header/index.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { AuthContext } from "../../../App";
+
+export const Header = () => {
+    const { isAuth, setIsAuth } = useContext(AuthContext)
+
+    return (
+        <div className="header">
+            <Link to='/'>Main</Link>
+            {!isAuth && (<Link to='/auth'>auth</Link>)}
+            {!isAuth && (<Link to='/register'>register</Link>)}
+            {isAuth && (<Link to='/profile'>profile</Link>)}
+            {isAuth && (<button onClick={()=>{setIsAuth(false)}}>quit</button>)}
+        </div>
+    );
+};
